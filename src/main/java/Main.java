54d4d782a586cdcaf822ecc027c45971c4070427
Artyel <artyel@icloud.com>
2023-03-06 12:49:18 +0000

    Fix #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-    BonusService service = new BonusService();
-    long amount = 1000;
-    boolean registered = false;
-    long bonus = service.calculate (amount, registered);
+        BonusService service = new BonusService();
+        long amount = 1000;
+        boolean registered = false;
+        long bonus = service.calculate(amount, registered);
         System.out.println(bonus);
     }
 }
